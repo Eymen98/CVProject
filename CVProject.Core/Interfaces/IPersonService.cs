131d34b3c72bf6178c550e7a602f00b873c8c2ab
Ayman <Ayman@DESKTOP-7JV00L4>
2024-10-12 11:47:53 +0300

    Add project files.

---
+++ CVProject.Core/Interfaces/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using CVProject.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVProject.Core.Interfaces
+{
+    public interface IPersonService
+    {
+        public Person GetPerson(int id);
+
+        public int CreatePerson(Person person);
+        
+    }
+}
---
+++ CVProject.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CVProject.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVProject.Infrastructure.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Person> Persons { get; set; }
+    }
+}
