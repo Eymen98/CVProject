0e8a1a09c95431ba9c94bca0dd03bdf5f55618e6
Ayman <ayman.dashak1998@gmail.com>
2024-10-14 21:14:28 +0300

    test

--- CVProject.Core/Entities/Person.cs
+++ CVProject.Core/Entities/Person.cs
@@ -10,7 +10,8 @@ namespace CVProject.Core.Entities
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? SurName { get; set; }
-        public string? JobTitle { get; set; }
+        public string SurName { get; set; }
+        public string JobTitle { get; set; }
+        
     }
 }
