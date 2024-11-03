49efdb9d9c01fcf30db2b6eb2265b9a3c9625124
Ayman <ayman.dashak1998@gmail.com>
2024-11-03 18:01:32 +0300

    altıyapısı tamamlandı

---
+++ CvProject.Web.Angular/CvProject.Web.Angular.Server/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using CVProject.Core.Entities;
+using CVProject.Core.Interfaces.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvProject.Web.Angular.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private readonly IPersonRepository _personRepository;
+
+        public PersonController(ILogger<PersonController> logger, IPersonRepository personRepository)
+        {
+            _logger = logger;
+            _personRepository = personRepository;
+        }
+        [HttpGet("GetPersonInformations")]
+        public Person GetPersonInformations()
+        {
+            var person=_personRepository.FindOne(x=> x.Id == 1);
+            return person;
+        }
+    }
+}
